b64ba5a4f18bb805cb73c05ec649be12a871ae62
Aditi <asaki444@gmail.com>
2020-03-23 20:45:08 -0400

    migrated the table

--- back_end_rails_api/app/controllers/transactions_controller.rb
+++ back_end_rails_api/app/controllers/transactions_controller.rb
@@ -7,7 +7,7 @@ class TransactionsController < ApplicationController
             if balance > 0 
                @current_user.update(account_balance: balance)
                stock = Stock.create(amount_of_stock: params["transaction"]["amount_of_stock"], stock_symbol: params["transaction"]["stock_symbol"])
-               Transaction.create(user_id: @current_user.id, stock_id: stock.id, params["transaction"]["purchase_amount"])
+               Transaction.create(user: @current_user, stock: stock params["transaction"]["purchase_amount"])
             else
                 render json:{
                     status: 405
--- back_end_rails_api/db/migrate/20200323094950_create_transactions.rb
+++ back_end_rails_api/db/migrate/20200323094950_create_transactions.rb
@@ -1,8 +1,8 @@
 class CreateTransactions < ActiveRecord::Migration[5.0]
   def change
     create_table :transactions do |t|
-      t.references, :user_id, foreign_key: true
-      t.references, :stock_id, foreign_key: true
+      t.references :user, foreign_key: true
+      t.references :stock, foreign_key: true
       t.integer :purchase_amount
 
       t.timestamps
