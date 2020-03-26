52b2bfe5dd8831ac097fac5254b9502cbb86182e
Aditi <asaki444@gmail.com>
2020-03-26 02:39:10 -0400

    fixed error on transactions controller

--- back_end_rails_api/app/controllers/transactions_controller.rb
+++ back_end_rails_api/app/controllers/transactions_controller.rb
@@ -3,13 +3,13 @@ class TransactionsController < ApplicationController
     
     def create
         if @current_user 
-            balance = @current_user.account_balance - (params["transaction"]["purchase_amount"])
+            balance = @current_user.account_balance - params["transaction"]["purchase_amount"]
             if balance > 0 
                @current_user.update(account_balance: balance)
                stock = Stock.create(amount_of_stock: params["transaction"]["amount_of_stock"], stock_symbol: params["transaction"]["stock_symbol"])
-               Transaction.create(user: @current_user, stock: stock params["transaction"]["purchase_amount"])
+               Transaction.create(user: @current_user, stock: stock)
             else
-                render json:{
+                render json: {
                     status: 405
                     message: "Cannot process request due to insufficient funds" 
                 }
@@ -20,9 +20,11 @@ class TransactionsController < ApplicationController
     def index
         if @current_user
             transactions = @current_user.transactions
-            render json: => transactions
+            render json: {
+                transactions: transactions
+            }
         else
-            render json: => {
+            render json: {
                 status: 403
             }
         end
