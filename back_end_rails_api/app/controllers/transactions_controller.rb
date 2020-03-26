70acc7127f14d1e8d1a91c328cbdd639a96ee1d0
Aditi <asaki444@gmail.com>
2020-03-26 03:54:03 -0400

    adjusted controller, and models due to errors

--- back_end_rails_api/app/controllers/transactions_controller.rb
+++ back_end_rails_api/app/controllers/transactions_controller.rb
@@ -6,11 +6,22 @@ class TransactionsController < ApplicationController
             balance = @current_user.account_balance - params["transaction"]["purchase_amount"]
             if balance > 0 
                @current_user.update(account_balance: balance)
-               stock = Stock.create(amount_of_stock: params["transaction"]["amount_of_stock"], stock_symbol: params["transaction"]["stock_symbol"])
+               symbol = params["transaction"]["stock_symbol"]
+               found_stock = stock_exists = Stock.find(stock_symbol: symbol)
+               if found_stock
+                new_amount = found_stock.amount_of_stock + params["transaction"]["amount_of_stock"]
+                found_stock.update(amount_of_stock: new_amount)
+               else
+               stock = Stock.create(amount_of_stock: params["transaction"]["amount_of_stock"], stock_symbol: symbol )
                Transaction.create(user: @current_user, stock: stock)
+               render json: {
+                   message: "purchase successful",
+                   user: @current_user
+               }
+                 end
             else
                 render json: {
-                    status: 405
+                    status: 405,
                     message: "Cannot process request due to insufficient funds" 
                 }
             end
