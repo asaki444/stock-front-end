b6e8027277bb582de21c34a29aff389c9ea5e169
Aditi <asaki444@gmail.com>
2020-03-29 22:09:53 -0400

    updated urls for deployed backend

--- front_end_react_app/src/globalFunctions/apiFunctions.js
+++ front_end_react_app/src/globalFunctions/apiFunctions.js
@@ -1,7 +1,7 @@
 import axios from "axios";
+const backend_url = "https://fathomless-wave-97372.herokuapp.com"
 
 export function apiAlphaRequest(urlPurpose,val){
-   console.log("this fired")
    let urlFunction,param;
    if(urlPurpose === "search"){
     urlFunction = 'SYMBOL_SEARCH'
@@ -19,10 +19,10 @@ export function sessionRequest(typeOfLogin, objToPost){
    let url;
    
    if(typeOfLogin === 'registration'){
-       url = 'http://localhost:3001/registrations'
+       url = 'registrations'
    }
    else if(typeOfLogin === 'login'){
-      url = 'http://localhost:3001/sessions'
+      url =   `${backend_url}/sessions`
    }
  
      return axios.post(url,
@@ -31,9 +31,9 @@ export function sessionRequest(typeOfLogin, objToPost){
 }
 
 export function transactionRequest(objToPost){
-    return axios.post('http://localhost:3001/transactions', objToPost, {withCredentials: true})
+    return axios.post(`${backend_url}/transactions`, objToPost, {withCredentials: true})
 }
 
 export function logoutRequest(){
-  return axios.delete('http://localhost:3001/logout')
+  return axios.delete(`${backend_url}/logout`)
 }
